dde499612680c13eccee020bcd2d5264778cf69c
Laura Valentina Uscategui <laura.uscategui-c@mail.escuelaing.edu.co>
2025-02-06 17:06:26 -0500

    transformada

--- LAMBDAS/FT.py
+++ LAMBDAS/FT.py
@@ -1,44 +1,26 @@
 """Acá estará la TF"""
-import json
 import math
 import cmath  
 
-def transformada_fourier(x):
+def dft(x):
     N = len(x)
-    Y = []
+    Y = []  # almacenar la salida 
 
-    for k in range(N):  
+    for k in range(N):  # Para cada frecuencia k de salida
         sum_complej = 0  
-        for n in range(N):  
-            ang = -2j * math.pi * k * n / N  
-            sum_complej += x[n] * cmath.exp(ang) 
-        Y.append(sum_complej)  
-    return [{"real": val.real, "imag": val.imag} for val in Y]  
-
-def cargar_señal(event):
-    """Carga la señal desde el JSON recibido en AWS Lambda."""
-    try:
-        if "body" in event:
-            data = json.loads(event["body"])  # Si viene como string dentro de "body"
-        else:
-            data = event  # Si ya es un diccionario
+        for n in range(N):  # Para cada muestra de la señal de entrada
+            ang = -2j * math.pi * k * n / N  # exponente complejo
+            sum_complej += x[n] * cmath.exp(ang)  #  fórmula fourier discreta
         
-        return [float(value) for value in data["signal"]]  # Convertir valores a flotantes
-    except Exception as e:
-        return None
+        Y.append(sum_complej)  
+    
+    return Y  
 
-def lambda_handler(event, context):
-    """Función Lambda para procesar la señal desde JSON."""
-    signal = cargar_señal(event)
+# Señal ejemplo clase
+x = [1, 2, 3, 1, 1, 2]
 
-    if signal is None:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': 'Formato de entrada incorrecto. Asegúrate de enviar un JSON válido con "signal".'})
-        }
+Y = dft(x)
 
-    resultado = transformada_fourier(signal)  
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'DFT': resultado})
-    }
+for k in range(len(Y)):  
+    val = Y[k]
+    print(f"Y[{k}] = {val.real:.4f} + {val.imag:.4f}j")
