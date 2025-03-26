b31a4957e0efb6839c0b8c24e5759abe41cce3e4
natalia17rios <natalia17rios@gmail.com>
2025-03-26 17:19:59 -0500

    commit

--- LAMBDAS/Filtro_digital.py
+++ LAMBDAS/Filtro_digital.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def filtro_fir_pbajas(signal, fs, fc, L=101):
@@ -41,3 +42,47 @@ def generar_senal(fs, T, f1, f2, A1, A2):
     N = int(fs * T)
     signal = [A1 * math.sin(2 * math.pi * f1 * t / fs) + A2 * math.sin(2 * math.pi * f2 * t / fs) for t in range(N)]
     return signal
+
+# PARÁMETROS:
+
+fs = 500      # Frecuencia de muestreo en Hz
+T = 0.5       # Duración de la señal 
+f1 = 10       # Frecuencia 1 
+f2 = 50       # Frecuencia 2 
+A1 = 1        # Amplitud 1
+A2 = 0.5      # Amplitud 2
+
+f_corte = 30  # Frecuencia de corte (pasa bajas/altas)
+f_low_ = 40 #Límite inferior (pasa/rechaza banda)
+f_high_ = 60 #Límite superior (pasa/rechaza banda)
+L = 101       # Número de coeficientes del filtro
+tipo_filtro = "bandstop"  # "lowpass", "highpass", "bandpass", "bandstop"
+
+
+signal = generar_senal(fs, T, f1, f2, A1, A2)
+
+if tipo_filtro == "lowpass":
+    signal_filtrada, h = filtro_fir_pbajas(signal, fs, f_corte, L)
+elif tipo_filtro == "highpass":
+    signal_filtrada, h = filtro_fir_paltas(signal, fs, f_corte, L)
+elif tipo_filtro == "bandpass":
+    signal_filtrada, h = filtro_fir_pbanda(signal, fs, f_low_, f_high_, L)
+elif tipo_filtro == "bandstop":
+    signal_filtrada, h = filtro_fir_rbanda(signal, fs, f_low_, f_high_, L)
+else:
+    raise ValueError("Tipo de filtro no reconocido.")
+
+
+plt.figure(figsize=(12, 6))
+plt.subplot(2, 1, 1)
+plt.plot(signal, label="Señal Original")
+plt.legend()
+plt.title("Señal Original")
+
+plt.subplot(2, 1, 2)
+plt.plot(signal_filtrada, label=f"Señal Filtrada FIR ({tipo_filtro})", color="red")
+plt.legend()
+plt.title("Señal Filtrada")
+
+plt.tight_layout()
+plt.show()
